0b68cc7ca838cb15da4b46f8823019987814be77
zackees <z@zackees.com>
2022-04-27 14:51:58 -0400

    update

--- pyjpgclipboard/__init__.py
+++ pyjpgclipboard/__init__.py
@@ -10,5 +10,8 @@ import sys
 if sys.platform == "darwin":
     from .clipboard_darwin import clipboard_dump_jpg_darwin as clipboard_dump_jpg
     from .clipboard_darwin import clipboard_load_jpg_darwin as clipboard_load_jpg
+elif sys.platform == "win32":
+    from .clipboard_win32 import clipboard_dump_jpg_win32 as clipboard_dump_jpg
+    from .clipboard_win32 import clipboard_load_jpg_win32 as clipboard_load_jpg
 else:
     raise NotImplementedError(f"Unsupported platform: {sys.platform}")
---
+++ pyjpgclipboard/clipboard_win32.py
@@ -0,0 +1,13 @@
+"""
+    Win32 implementation of clipboard functions for copy paste jpg.
+"""
+
+
+def clipboard_load_jpg_win32(jpeg_path: str) -> None:
+    """Copies the image at the given path into the system clipboard."""
+    raise NotImplementedError("Not implemented on win32 yet")
+
+
+def clipboard_dump_jpg_win32(jpeg_path: str) -> None:
+    """Copies the image at the given path into the system clipboard."""
+    raise NotImplementedError("Not implemented on win32 yet")
