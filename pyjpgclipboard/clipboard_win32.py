6364c23593eb08b7f9aab86c572bc8e2f0c67d67
zackees <z@zackees.com>
2022-04-27 15:05:05 -0400

    adds win32

--- pyjpgclipboard/clipboard_win32.py
+++ pyjpgclipboard/clipboard_win32.py
@@ -2,6 +2,12 @@
     Win32 implementation of clipboard functions for copy paste jpg.
 """
 
+# flake8: noqa
+# pylint: disable=all
+
+import win32clipboard  # type: ignore
+import win32con  # type: ignore
+
 
 def clipboard_load_jpg_win32(jpeg_path: str) -> None:
     """Copies the image at the given path into the system clipboard."""
