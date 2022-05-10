eeb5574d500e00455eca1280a760f8c78f08b555
Zach Vorhies <z@zackees.com>
2022-05-10 10:48:47 -0700

    better query for display on linux

--- pyjpgclipboard/clipboard_linux.py
+++ pyjpgclipboard/clipboard_linux.py
@@ -3,18 +3,19 @@
 """
 
 
+import os
 import subprocess
+
+DISPLAY = os.environ.get("DISPLAY", ":0")
 
 
 def clipboard_load_jpg_linux(jpeg_path: str) -> None:
     """Copies the image at the given path into the system clipboard."""
-    cmd = f'xclip -d :0 -selection clipboard -t image/jpeg -i "{jpeg_path}"'
-    print(cmd)
+    cmd = f'xclip -d {DISPLAY} -selection clipboard -t image/jpeg -i "{jpeg_path}"'
     subprocess.run(cmd, shell=True, check=False)
 
 
 def clipboard_dump_jpg_linux(jpeg_path: str) -> None:
     """Dumps the image contents into the currnet path."""
-    cmd = f'xclip -d :0 -selection clipboard -t image/jpeg -o > "{jpeg_path}"'
-    print(cmd)
+    cmd = f'xclip -d {DISPLAY} -selection clipboard -t image/jpeg -o > "{jpeg_path}"'
     subprocess.run(cmd, shell=True, check=False)
